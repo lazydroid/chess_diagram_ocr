3f4de2c85bedfd468269031357091039030b9e56
lenik terenin <lenik@lazydroid.com>
2021-01-06 03:03:23 +0900

    multiple inputs, ignore empty squares

--- board_splitter.py
+++ board_splitter.py
@@ -38,58 +38,58 @@ if __name__ == '__main__':
 	if len(sys.argv) < 2 :
 		sys.exit('Need an argument: ./board_splitter.py FOLDER')
 
-	pages = sys.argv[1]
-	if not os.path.isdir(pages) :
-		sys.exit('Not a folder or does not exist: ' + folder)
+	for pages in sys.argv[1:] :
+		if not os.path.isdir(pages) :
+			sys.exit('Not a folder or does not exist: ' + folder)
 
-	r, d, files = os.walk(pages).next()
-	for f in files :
-		#f = 'dee3ed3205b763ac0ad29cf218b93810-120.png'
-		#f = '4e53bead1775de509de92091ab72a583-57.png'
-		print( f )
-		name = os.path.join( r, f )
+		r, d, files = os.walk(pages).next()
+		for f in files :
+			#f = 'dee3ed3205b763ac0ad29cf218b93810-120.png'
+			#f = '4e53bead1775de509de92091ab72a583-57.png'
+			print( f )
+			name = os.path.join( r, f )
 
-		base = f.split('/')[-1].split('.')[0]
+			base = f.split('/')[-1].split('.')[0]
 
-		img = cv2.imread( name )
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			img = cv2.imread( name )
+			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-		offset = 5
-		size_x = img.shape[0] - offset*2
-		size_y = img.shape[1] - offset*2
+			offset = 5
+			size_x = img.shape[0] - offset*2
+			size_y = img.shape[1] - offset*2
 
-		for i in range(9) :
-			start_point = (0, offset + i*size_x / 8)
-			end_point = (img.shape[1], offset + i*size_x / 8)
-			cv2.line( img, start_point, end_point, (0, 255, 0), 1 )
+			for i in range(9) :
+				start_point = (0, offset + i*size_x / 8)
+				end_point = (img.shape[1], offset + i*size_x / 8)
+				cv2.line( img, start_point, end_point, (0, 255, 0), 1 )
 
-			start_point = (offset + i*size_y / 8, 0)
-			end_point = (offset + i*size_y / 8, img.shape[0])
-			cv2.line( img, start_point, end_point, (0, 0, 255), 1 )
+				start_point = (offset + i*size_y / 8, 0)
+				end_point = (offset + i*size_y / 8, img.shape[0])
+				cv2.line( img, start_point, end_point, (0, 0, 255), 1 )
 
-		empty_count = 0
-		for i in range(8) :
-			for j in range(8) :
-				left = offset + i*size_y/8
-				right = offset + (i+1)*size_y/8
-				top = offset + j*size_x/8
-				bottom = offset + (j+1)*size_x/8
-				square = gray[top:bottom, left:right]
-				empty = is_empty( square )
-				if empty :
-					empty_count += 1
-					empty_count &= 15
-				if empty_count == 0 or not empty :
-					save_square( base, i*8+j, square )
-				#print( np.mean( square ), is_empty(square), end = '')
-			print()
+			empty_count = 0
+			for i in range(8) :
+				for j in range(8) :
+					left = offset + i*size_y/8
+					right = offset + (i+1)*size_y/8
+					top = offset + j*size_x/8
+					bottom = offset + (j+1)*size_x/8
+					square = gray[top:bottom, left:right]
+					empty = is_empty( square )
+					if empty :
+						empty_count += 1
+						empty_count &= 31
+					if empty_count == 0 or not empty :
+						save_square( base, i*8+j, square )
+					#print( np.mean( square ), is_empty(square), end = '')
+				print()
 
-		cv2.imshow('squares', cv2.pyrUp(img))
-		ch = cv2.waitKey() & 0xFF
-		if ch == 27:
-			break
+			#cv2.imshow('squares', cv2.pyrUp(img))
+			#ch = cv2.waitKey() & 0xFF
+			#if ch == 27:
+			#	break
 
-		#break
+			#break
 
 	cv2.destroyAllWindows()
 
